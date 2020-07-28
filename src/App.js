d9a402c63d2a7e0b13a962ed9800e85cd6820ab8
Jacob Cunningham <jcun4128@gmail.com>
2020-07-28 17:00:45 -0500

    working on basic search interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import SearchInterface from './search-interface/SearchInterface';
 
-function App() {
+const App = () => {
+  // const getData = () => {
+  //   axios.get(targetUrl)
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response);
+  //       if (response.data && response.data.data) {
+  //         console.log(response.data);
+  //       }
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .then(function () {
+  //       // always executed
+  //     });
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="subreddit-topic-search">
+      <SearchInterface />
     </div>
   );
 }
---
+++ src/search-interface/SearchInterface.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './SearchInterface.scss';
+import sampleData from '../sample-data.json';
+
+const SearchInterface = (props) => {
+    // const { defaultSearchUrl } = props;
+    
+    const [searchUrl, setSearchUrl] = useState('https://www.reddit.com/r/shopify/new/.json');
+    const [searchStr, setSearchStr] = useState('');
+
+    const changeSearchStr = (e) => {
+        setSearchStr(e.target.value);
+    }
+
+    const changeSearchUrl = (e) => {
+        setSearchUrl(e.target.value);
+    }
+
+    const searchResults = () => {
+        return null;
+    }
+
+    const parseSampleData = () => {
+        const newPosts = sampleData.data.children;
+        console.log(newPosts);
+    }
+    
+    useEffect(() => {
+        parseSampleData(); // response.data
+    }, []);
+
+    return <div className="subreddit-topic-search__search-interface">
+        <input type="text" value={searchUrl} onChange={(e) => changeSearchUrl(e) } />
+        <textarea value={searchStr} onChange={(e) => changeSearchStr(e)} placeholder='Enter search terms separated by commas' />
+        { searchResults() }
+    </div>
+}
+
+export default SearchInterface;
